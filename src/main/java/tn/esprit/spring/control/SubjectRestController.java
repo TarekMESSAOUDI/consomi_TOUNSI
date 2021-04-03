e87227c74b2a2c8fcf54746dd28f1a497b8f5c1f
Tarek MESSAOUDI <57331210+TarekMESSAOUDI@users.noreply.github.com>
2021-04-03 16:47:55 +0100

    Security

--- src/main/java/tn/esprit/spring/control/SubjectRestController.java
+++ src/main/java/tn/esprit/spring/control/SubjectRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import tn.esprit.spring.entities.Subject;
 import tn.esprit.spring.service.ISubjectService;
 
-@RestController
+@Controller
 public class SubjectRestController {
 	
 	@Autowired
