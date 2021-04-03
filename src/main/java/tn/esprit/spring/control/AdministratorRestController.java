88f7386a86b2590ae49bef013d400a04eafe513d
Tarek MESSAOUDI <57331210+TarekMESSAOUDI@users.noreply.github.com>
2021-04-03 15:07:17 +0100

    CRUD

--- src/main/java/tn/esprit/spring/control/AdministratorRestController.java
+++ src/main/java/tn/esprit/spring/control/AdministratorRestController.java
@@ -2,7 +2,7 @@ package tn.esprit.spring.control;
 
 import java.util.List;
 import java.util.Optional;
-//test
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
