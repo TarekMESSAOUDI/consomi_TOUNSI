70275a757918ed14ce034d1e7c05da0928a30c2c
Tarek MESSAOUDI <57331210+TarekMESSAOUDI@users.noreply.github.com>
2021-04-05 17:18:54 +0100

    affect image t client

---
+++ src/main/java/tn/esprit/spring/service/IImageUserService.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring.service;
+
+
+public interface IImageUserService {
+
+	void affectationImageToClient(int idImageUser, int idUser);
+	
+
+	//void affecterImageAClient(int idImageUser, int idUser);
+	
+	
+
+}
